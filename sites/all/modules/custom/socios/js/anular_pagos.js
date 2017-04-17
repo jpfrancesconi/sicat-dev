979134f9a655170e7ef1e44e1a2d731eb1c527bc
franIdeasOrange <giudici.francisco@gmail.com>
2017-04-17 12:06:38 -0300

    Commit Fran 17/04/2017

--- sites/all/modules/custom/socios/js/anular_pagos.js
+++ sites/all/modules/custom/socios/js/anular_pagos.js
@@ -11,6 +11,35 @@
                 return false;
                 });
             });
+            $('#listado-pagos-socio .form-type-checkbox input').click(function(e) {
+                    var total = 0;
+                    var check = 0;
+             
+                    $("#listado-pagos-socio table tbody tr").each(function (index) 
+                    {                         
+                        $(this).children("td").each(function (index2) 
+                        {       
+                        console.log($(this).children('td .form-type-checkbox input'));                 
+                            if(index2==0)
+                            {      
+                                if($(this).children('td .form-type-checkbox input').context.children["0"].children["0"].checked) {                                
+                                    check = 1;                                
+                                }
+                            }
+                            if(index2 == 5 && check == 1)
+                            {
+                                valor = $(this).children('td .form-type-checkbox input').context.childNodes["0"].nodeValue;
+                                valor = parseInt(valor);
+
+                                total = total + valor;
+                                check= 0;
+                            }
+                        })                    
+                    });
+                    $('#edit-total').val(total);
+                                                                                
+            });
         }			
     } 
 }(jQuery));
+
--- sites/all/modules/custom/socios/js/socios.js
+++ sites/all/modules/custom/socios/js/socios.js
@@ -1,20 +1,22 @@
-/*(function ($) {
+(function ($) {
   Drupal.behaviors.socios = {
     attach: function (context, settings) {
-		$(document).ready(function(event){
-			$.ajax({
-            type: 'POST',
-            url: 'ajax.php',
-            data: 'id=testdata',
-            dataType: 'json',
-            cache: false,
-            success: function(result) {
-                $('#content1').html(result[0]);
-            },
-        	});
+		$(document).one('ready', function(event){
+			//obtenemos el valor
+            var tipo_socio = $("#edit-tipo-socio").val();
+            var forma_pago = $("#edit-forma-pago").val();
+            $('#edit-forma-pago').prop('disabled', true);
+            if(tipo_socio != 2 && forma_pago != 1)
+            {
+                $(".form-item.form-type-textfield.form-item-lugar-cobro").css("display","none");
+                $(".form-item.form-type-select.form-item-personal").css("display","none");
+            }
+            setTimeout(function() {
+               $('#edit-forma-pago').prop('disabled', false);
+            }, 6000);
 		});
     }
   };
-}(jQuery));*/
+}(jQuery));
 
 
